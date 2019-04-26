5b8fd62122fa7fa5a3b483116594a2f39fec97ae
U-EKRA\EM-Doronina <EM-Doronina@EM-DORONINA.ekra.local>
2019-04-26 16:09:59 +0300

    Update .travis.yml

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -5,8 +5,6 @@
 
 var webpackConfig = require('../../build/webpack.test.conf')
 
-process.env.CHROME_BIN = '/usr/bin/google-chrome'
-
 module.exports = function karmaConfig (config) {
   config.set({
     // to run in additional browsers:
@@ -14,9 +12,7 @@ module.exports = function karmaConfig (config) {
     //    http://karma-runner.github.io/0.13/config/browsers.html
     // 2. add it to the `browsers` array below.
 
-    //browsers: ['PhantomJS'],
-    browsers: ['Chrome'],
-
+    browsers: ['PhantomJS'],
     frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
     reporters: ['spec', 'coverage'],
     files: ['./index.js'],
