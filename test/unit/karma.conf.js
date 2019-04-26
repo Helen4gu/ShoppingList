97bf010f3f8f0c0c5637eb9f8b1a0ff1793e9bcc
U-EKRA\EM-Doronina <EM-Doronina@EM-DORONINA.ekra.local>
2019-04-26 15:49:00 +0300

    Modify .travis.yml

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -5,32 +5,15 @@
 
 var webpackConfig = require('../../build/webpack.test.conf')
 
-process.env.CHROME_BIN = require('puppeteer').executablePath()
-
-
 module.exports = function karmaConfig (config) {
   config.set({
     // to run in additional browsers:
     // 1. install corresponding karma launcher
     //    http://karma-runner.github.io/0.13/config/browsers.html
     // 2. add it to the `browsers` array below.
-    
+
     //browsers: ['PhantomJS'],
-    browsers: ['MyHeadlessChrome'],
-    customLaunchers: {
-      MyHeadlessChrome: {
-        base: 'ChromeHeadless',
-        /*flags: [
-          '--no-sandbox',
-          '--disable-gpu', 
-          '--remote-debugging-port=9222',
-          '--no-proxy-server',
-          '--use-mock-keychain',
-          '--disable-setuid-sandbox',
-          '--disable-web-security'
-        ]*/
-      }
-    },
+    browsers: ['Chrome'],
 
     frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
     reporters: ['spec', 'coverage'],
