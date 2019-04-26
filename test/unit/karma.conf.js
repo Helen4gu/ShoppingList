23090de1d10ddadc090ed0f3442b3d9d46014588
U-EKRA\EM-Doronina <EM-Doronina@EM-DORONINA.ekra.local>
2019-04-26 15:53:49 +0300

    Update .travis.yml

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function karmaConfig (config) {
     // 2. add it to the `browsers` array below.
 
     //browsers: ['PhantomJS'],
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
 
     frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
     reporters: ['spec', 'coverage'],
